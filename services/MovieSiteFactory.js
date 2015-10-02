153c74d7eeadb426431fa633110613e7377b14ca
alexandra.brown <redrose@redroses-imac.local>
2015-10-02 11:02:49 -0700

    fix but with factory

--- services/MovieSiteFactory.js
+++ services/MovieSiteFactory.js
@@ -4,5 +4,12 @@ nicksFlicks.factory('MovieSiteFactory', function MovieSiteFactory() {
     {title: "Alien XXI Is The WORST",
       description: "After watching the first 20 Alien movies and loving every second of it, i was convinced that I would love this one too. Unfortunatly, that was not the case. My dreams are ruined. My life is ruined. Everything is ruined."}
   ];
+
+  factory.addReview = function() {
+    var review = { title: factory.title, description: factory.description };
+    factory.review.push(review);
+    factory.reviewTitle = null;
+    factory.reviewDescription = null;
+  };
   return factory;
 });
